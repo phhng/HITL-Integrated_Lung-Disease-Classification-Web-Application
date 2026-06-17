6f7e6b79d131d7f9304885da491dce46e95abc6b
Phuong <hphuong11204@gmail.com>
2026-06-17 13:55:20 +0700

    minor edit, adding pending, feedback and uncertain dir

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Create VENV on version 3.11 of python
 py -3.11 -m venv myenv
 
-# Install trained Keras models
-
 # Activate VENV
 .\myenv\Scripts\activate
 
--- app/utils/api.py
+++ app/utils/api.py
@@ -7,11 +7,28 @@ import matplotlib.pyplot as plt
 import traceback
 import uuid
 from datetime import datetime
+import csv 
 
 IMAGE_SIZE = (224, 224)
 current_dir = os.path.dirname(__file__)
 
 DATASET_DIR = "dataset"
+UNCERTAIN_DIR = os.path.join(
+    "dataset",
+    "uncertain"
+)
+UNCERTAIN_DIR = os.path.abspath(UNCERTAIN_DIR)
+FEEDBACK_DIR = os.path.join(
+    "dataset",
+    "feedback"
+)
+FEEDBACK_DIR = os.path.abspath(FEEDBACK_DIR)
+PENDING_DIR = os.path.join(
+    "dataset",
+    "pending"
+)
+PENDING_DIR = os.path.abspath(PENDING_DIR)
+PENDING_FEEDBACK = None
 
 MODEL_CONFIGS = {
     "Kermany": {
@@ -27,7 +44,7 @@ MODEL_CONFIGS = {
         "filename": "cov_model_42.keras",
         "num_classes": 4,
         "classes": {
-            "covid": "COVID-19",
+            "covid-19": "COVID-19",
             "lung_opacity": "Mờ phổi",
             "normal": "Bình thường",
             "pneumonia": "Viêm phổi"
@@ -37,7 +54,7 @@ MODEL_CONFIGS = {
         "filename": "cov_masked_42.keras",
         "num_classes": 4,
         "classes": {
-            "covid": "COVID-19",
+            "covid-19": "COVID-19",
             "lung_opacity": "Mờ phổi",
             "normal": "Bình thường",
             "pneumonia": "Viêm phổi"
@@ -103,12 +120,37 @@ def run_prediction(model_key,image,method):
         topk=len(active["class_names"])
     )
 
-def predict_ui(model_name,image,method):
+def predict_ui(model_name,image,method,pending_path):
+    global PENDING_FEEDBACK
+    active = AVAILABLE_MODELS[model_name]
+
+    save_pending_if_abandoned()
+
     heatmap, pred, conf, topk_table, fig = run_prediction(
         model_name,
         image,
         method
     )
+    display_to_id = {
+        v: k for k, v in active["classes"].items()
+    }
+
+    pred_key = display_to_id[pred]
+    # pred_key = normalize_class_name(pred)
+
+    PENDING_FEEDBACK = {
+        "image": image,
+        "prediction": pred_key,
+        "confidence": conf
+    }
+
+    if conf < 0.70:
+        save_prediction(
+            image,
+            pred_key,
+            UNCERTAIN_DIR,
+            conf * 100
+        )
 
     return (
         heatmap,
@@ -120,23 +162,52 @@ def predict_ui(model_name,image,method):
     )
 
 
-def save_feedback(image,class_name):
-    class_name = normalize_class_name(class_name)
-    class_dir = os.path.join(DATASET_DIR,class_name)
-
-    os.makedirs(class_dir,exist_ok=True)
-    filename = datetime.now().strftime("img_%y%m%d%H%M%S.png")
-    save_path = os.path.join(class_dir,filename)
-
-    image.save(save_path)
-
-    return save_path
-
 def submit_feedback(image,correct_class):
-    path = save_feedback(
-        image,
-        correct_class
-    )
+    global PENDING_FEEDBACK
 
+    path = save_prediction(
+        image,
+        correct_class,
+        FEEDBACK_DIR
+    )
+    PENDING_FEEDBACK = None
     # return f"Saved to {path}"
     return f"Lưu về {path}"
+
+def save_prediction(image,pred_class,root_dir,confidence=None):
+    class_dir = os.path.join(root_dir, pred_class)
+
+    os.makedirs(class_dir, exist_ok=True)
+
+    uid = uuid.uuid4().hex[:8]
+
+    filename = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    if confidence is not None:
+        filename += f"_conf{confidence:.1f}"
+
+    filename += f"_{uid}.png"
+
+    save_path = os.path.join(class_dir, filename)
+
+    image.save(save_path)
+    return save_path
+
+def save_pending_if_abandoned():
+    global PENDING_FEEDBACK
+
+    if not PENDING_FEEDBACK:
+        return
+
+    required_keys = ["image", "prediction", "confidence"]
+
+    if not all(k in PENDING_FEEDBACK for k in required_keys):
+        return
+    save_prediction(
+        image=PENDING_FEEDBACK["image"],
+        pred_class=PENDING_FEEDBACK["prediction"],
+        confidence=PENDING_FEEDBACK["confidence"] * 100,
+        root_dir=PENDING_DIR
+    )
+
+    PENDING_FEEDBACK = None
